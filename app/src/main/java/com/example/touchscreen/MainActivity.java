f2ea663e52005cb7fb3f08e317d95922e06863f9
azarrias <adolfo.zarrias@gmail.com>
2019-04-18 20:11:56 +0200

    Capture touch events from the touch screen

--- app/src/main/java/com/example/touchscreen/MainActivity.java
+++ app/src/main/java/com/example/touchscreen/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.touchscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvInput = findViewById(R.id.tv_input);
+        tvInput.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        TextView tvOutput = findViewById(R.id.tv_output);
+        tvOutput.append(event.toString() + "\n");
+        return true;
     }
 }
